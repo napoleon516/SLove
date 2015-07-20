3b9ce8ac388cf908a592e521c963fd6c51820cc9
root <ltf566@163.com>
2015-07-20 21:21:15 +0800

    init the 5ZTJ web app

--- forum/context_processors.py
+++ forum/context_processors.py
@@ -5,7 +5,7 @@ def custom_proc(request):
         navigation_bar = [
             ('/', 'topic', '社区'),
             ('/members/', 'members', '成员'),
-            ('/static/pages/timeline/index.html', 'timeline', '大事记'),
-            ('/static/pages/nav/index.html', 'nav', '导航'),
+            #('/static/pages/timeline/index.html', 'timeline', '大事记'),
+            #('/static/pages/nav/index.html', 'nav', '导航'),
         ],
     )
